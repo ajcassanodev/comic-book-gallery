ead519418520d1de1798286442c7d5c779bdf930
ajcassanodev <ajcassanodev@users.noreply.github.com>
2018-03-08 16:08:58 -0500

    Updated comicbook detail view

--- src/ComicBookGallery1/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery1/Controllers/ComicBooksController.cs
@@ -10,8 +10,20 @@ namespace ComicBookGallery1.Controllers
     {
         public ActionResult Detail()
         {
-            return View();
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hour</p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delagdo",
+                "Letters: Chris Eliopoulos"
+            };
 
+            return View();
+            
         }
     }
 }
